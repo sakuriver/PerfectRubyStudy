c218a4579882ff0daf9a1a2e6e29922b538a6a6f
sakuriver <suzuerain@yahoo.co.jp>
2020-06-08 23:04:39 +0900

    ニュータイプかどうか出力できる得体のしれないものを出してみた

--- to_method_file.rb
+++ to_method_file.rb
@@ -1,2 +1,15 @@
-p 1.to_s
-p true.to_s
+class GundamHuman
+	@number
+	@is_newtype
+	def initialize(number, is_newtype)
+		@number = number
+		@is_newtype = is_newtype
+	end
+  def display
+		p @number.to_s
+		p @is_newtype.to_s
+  end
+end
+
+amuro = GundamHuman.new(1, true)
+amuro.display
