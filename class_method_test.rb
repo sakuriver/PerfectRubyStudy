2f71025f7e9ad141902b994701888ac850454ede
sakuriver <suzuerain@yahoo.co.jp>
2020-06-08 23:24:54 +0900

    クラスメソッドをガンダムネタで書き換え

---
+++ class_method_test.rb
@@ -0,0 +1,15 @@
+CharAznableMessage = 'Because he s a little boy.'
+p CharAznableMessage.class
+p String.ancestors
+
+RestartScenarioNumber = 10
+p RestartScenarioNumber.class
+p Fixnum.ancestors
+
+Lalah = true
+RambaLal = false
+p Lalah.class
+p RambaLal.class
+
+p nil.class
+p NilClass.ancestors
