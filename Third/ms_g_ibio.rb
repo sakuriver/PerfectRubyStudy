8920c51b35524d3aed7aff4bd18b0d9942d5e42f
SuzueKatsushige <61958858+SuzueKatsushige@users.noreply.github.com>
2020-06-11 20:32:04 +0900

    オプション武器を題材にしたプログラムを実装中

---
+++ Third/ms_g_ibio.rb
@@ -0,0 +1,10 @@
+
+def weapon_slot_message(pilot_name, option_weapon_name = "")
+	puts "unit_name #{unit_name} "
+	puts "weapon "
+end
+
+pilot_name = ""
+option_weapon_name = ""
+weapon_slot_message(pilot_name)
+weapon_slot_message(pilot_name, option_weapon_name)
