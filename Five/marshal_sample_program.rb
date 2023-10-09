fcee77524442acbff3dd52941329235712c2eefb
sakuriver <suzuerain@yahoo.co.jp>
2023-10-09 18:58:31 +0900

    Marshalクラス関連処理で圧縮されたファイルの展開後をログ記述

--- Five/marshal_sample_program.rb
+++ Five/marshal_sample_program.rb
@@ -6,6 +6,9 @@ require './scenario_standard'
 title_scene = GameStandard::Title.new("", "秋のデータ祭りなのだー")
 
 dump_data_load = Marshal.dump(title_scene)
+dump_app_data = Marshal.load(dump_data_load)
 
 log_sampler = AppLogging::ApplicationLogger.new
+# 送受信後のデータをログファイルに保存
 log_sampler.info(dump_data_load)
+log_sampler.info(dump_app_data)
